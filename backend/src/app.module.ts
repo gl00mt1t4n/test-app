20fcfd3176d395706112252abe44b82d95c34de8
gl00mt1t4n <gloomtitan1337@gmail.com>
2025-06-10 12:19:30 +0530

    remove mistakenly extra nest module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EvaluateController } from './evaluate/evaluate.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, EvaluateController],
   providers: [AppService],
 })
 export class AppModule {}
