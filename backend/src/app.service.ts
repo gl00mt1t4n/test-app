8cc894828189d890fd31317474a53e68b244c2fa
gl00mt1t4n <gloomtitan1337@gmail.com>
2025-06-10 16:37:37 +0530

    string tokenization function (no decimals)

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,15 +1,15 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException } from "@nestjs/common";
 
 // Types of characters we expect in the string. Either numbers or these symbols.
 export enum TokenType {
-  Number = 'Number',
-  Plus = '+',
-  Minus = '-',
-  Multiply = '*',
-  Divide = '/',
-  LPar = '(',
-  RPar = ')'
-};
+  Number = "Number",
+  Plus = "+",
+  Minus = "-",
+  Multiply = "*",
+  Divide = "/",
+  LPar = "(",
+  RPar = ")",
+}
 
 // General interface (like a struct) for Tokens. If they are numbers (enum type Number), they will have a value. Otherwise just a tokentype
 export interface Token {
@@ -17,15 +17,99 @@ export interface Token {
   value?: number;
 }
 
+function isDigit(ch: string): boolean {
+  return ch >= "0" && ch <= "9";
+}
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
   }
 
   tokenizeExpression(expression: string): Token[] {
     let tokens: Token[] = [];
     let i = 0;
+    while (i < expression.length) {
+      // Skip spaces
+      // Throw error if not a number/symbol
+      // read numbers (including decimals, but implement it later)
+      // handle symbols if they are in TokenType
+
+      let ch = expression[i];
+
+      if (ch === " ") {
+        i++;
+        continue;
+        // skips whitespaces
+      }
+
+      // digits
+      // it should work for consecutive digits as well
+      if (isDigit(ch)) {
+        let numberString = ch;
+        i++;
+        while (i < expression.length && isDigit(expression[i])) {
+          numberString += expression[i];
+          i++;
+        }
+        tokens.push({
+          type: TokenType.Number,
+          value: parseInt(numberString, 10),
+        });
+        continue;
+      }
+      
+      // general symbols
+      if (ch === '+') {
+        tokens.push({
+          type: TokenType.Plus,
+        });
+        i++;
+        continue;
+      }
+      else if (ch === '-') {
+        tokens.push({
+          type: TokenType.Minus,
+        });
+        i++;
+        continue;
+      }
+      else if (ch === '*') {
+        tokens.push({
+          type: TokenType.Multiply,
+        });
+        i++;
+        continue;
+      }
+      else if (ch === '/') {
+        tokens.push({
+          type: TokenType.Divide,
+        });
+        i++;
+        continue;
+      }
+      else if (ch === '(') {
+        tokens.push({
+          type: TokenType.LPar,
+        });
+        i++;
+        continue;
+      }
+      else if (ch === ')') {
+        tokens.push({
+          type: TokenType.RPar,
+        });
+        i++;
+        continue;
+      }
+
+      
+
+      // anything else should throw bad request error
+      throw new BadRequestException(`Unknown Character ${ch}`);
+    }
+
     return tokens;
   }
 
