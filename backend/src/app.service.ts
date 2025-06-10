9472511b8592c610e757bbc9633a50a528f29f23
gl00mt1t4n <gloomtitan1337@gmail.com>
2025-06-10 13:23:35 +0530

    tokenize expr initialization. Add tokentypes enum and struct/interface for general tokens

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+
+// Types of characters we expect in the string. Either numbers or these symbols.
+export enum TokenType {
+  Number = 'Number',
+  Plus = '+',
+  Minus = '-',
+  Multiply = '*',
+  Divide = '/',
+  LPar = '(',
+  RPar = ')'
+};
+
+// General interface (like a struct) for Tokens. If they are numbers (enum type Number), they will have a value. Otherwise just a tokentype
+export interface Token {
+  type: TokenType;
+  value?: number;
+}
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  tokenizeExpression(expression: string): Token[] {
+    let tokens: Token[] = [];
+    let i = 0;
+    return tokens;
   }
 
   evaluateExpression(expression: string): number {
