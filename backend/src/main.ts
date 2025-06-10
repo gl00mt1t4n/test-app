adb2cf7d9fc67dbb19f2bb99ed3ee06f6c5661c8
gl00mt1t4n <gloomtitan1337@gmail.com>
2025-06-10 23:12:34 +0530

    enable cross module calling in backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // enable cross module calling (interaction bw frontend and backend)
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
