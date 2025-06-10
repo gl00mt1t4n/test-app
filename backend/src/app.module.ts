43b90abab88ab2ee86c6783555560508f93605ef
gl00mt1t4n <gloomtitan1337@gmail.com>
2025-06-10 12:42:56 +0530

    remove mistaken excess imports in module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EvaluateController } from './evaluate/evaluate.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, EvaluateController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
